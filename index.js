49b3f0dc96fb9db2bc7c93a8952f6e58c82722f1
knowledgeaholic <93440270+knowledgeaholic@users.noreply.github.com>
2022-02-19 18:40:11 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    			var callElement = document.querySelectorAll(".code-block")
+				function setCookie(cname, cvalue, exdays) {
+					  const d = new Date();
+					  var setTimeCookie= d.setTime(d.getTime() + (exdays*1*60*60*1000));
+					  let expires = "expires="+ d.toUTCString();
+					  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+				}
+				for(var i = 0;i < callElement.length;i++ ){
+					callElement[i].onclick = function(){
+						setCookie("advertisement","yes",0.005)
+						callElement.forEach(e => e.remove());
+					}
+				}	
+				function getCookie(cname) {
+				  let name = cname + "=";
+				  let decodedCookie = decodeURIComponent(document.cookie);
+				  let ca = decodedCookie.split(';');
+				  for(let i = 0; i <ca.length; i++) {
+					let c = ca[i];
+					while (c.charAt(0) == ' ') {
+					  c = c.substring(1);
+					}
+					if (c.indexOf(name) == 0) {
+					  return c.substring(name.length, c.length);
+					}
+				  }
+				  return "";
+				}
+				console.log("cookie: " + getCookie("advertisement"))
+				if((getCookie("advertisement"))){
+					callElement.forEach(e => e.remove());
+				}
+		});
