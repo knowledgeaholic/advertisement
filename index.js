1c2a1f4f04f952e2835022c3e4572ca8c3c081d7
knowledgeaholic <93440270+knowledgeaholic@users.noreply.github.com>
2022-04-12 12:24:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-jQuery(document).ready(function($){
-	$(".code-block").addClass('code-block-style')
-	function setCookie(cname, cvalue, exdays) {
-		  const d = new Date();
-		  var setTimeCookie= d.setTime(d.getTime() + (exdays*1*60*60*1000));
-		  let expires = "expires="+ d.toUTCString();
-		  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-	}
-	function getCookie(cname) {
-	  let name = cname + "=";
-	  let decodedCookie = decodeURIComponent(document.cookie);
-	  let ca = decodedCookie.split(';');
-	  for(let i = 0; i <ca.length; i++) {
-		let c = ca[i];
-		while (c.charAt(0) == ' ') {
-		  c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-		  return c.substring(name.length, c.length);
-		}
-	  }
-	  return "";
-	}
-	if((getCookie("advertisement"))){
-		$('.code-block').remove();
-	}
-	$('.code-block iframe').iframeTracker({
-		blurCallback: function(event) {
-			setCookie("advertisement","yes",24)
-		},
-		outCallback: function(element, event) {
-			if((getCookie("advertisement"))){
-				$('.code-block').remove();
-			}
-		}
-	});
-});
+// jQuery(document).ready(function($){
+// 	$(".code-block").addClass('code-block-style')
+// 	function setCookie(cname, cvalue, exdays) {
+// 		  const d = new Date();
+// 		  var setTimeCookie= d.setTime(d.getTime() + (exdays*1*60*60*1000));
+// 		  let expires = "expires="+ d.toUTCString();
+// 		  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// 	}
+// 	function getCookie(cname) {
+// 	  let name = cname + "=";
+// 	  let decodedCookie = decodeURIComponent(document.cookie);
+// 	  let ca = decodedCookie.split(';');
+// 	  for(let i = 0; i <ca.length; i++) {
+// 		let c = ca[i];
+// 		while (c.charAt(0) == ' ') {
+// 		  c = c.substring(1);
+// 		}
+// 		if (c.indexOf(name) == 0) {
+// 		  return c.substring(name.length, c.length);
+// 		}
+// 	  }
+// 	  return "";
+// 	}
+// 	if((getCookie("advertisement"))){
+// 		$('.code-block').remove();
+// 	}
+// 	$('.code-block iframe').iframeTracker({
+// 		blurCallback: function(event) {
+// 			setCookie("advertisement","yes",24)
+// 		},
+// 		outCallback: function(element, event) {
+// 			if((getCookie("advertisement"))){
+// 				$('.code-block').remove();
+// 			}
+// 		}
+// 	});
+// });
