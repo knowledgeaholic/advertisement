6e93f71f4fb2ad8fdb5f1ce2ffb7cdfac27843b3
knowledgeaholic <93440270+knowledgeaholic@users.noreply.github.com>
2022-08-24 08:48:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-	alert("Hello")
+	console.log("Hello")
 	var modal = document.getElementById("myModal");
 	var min = 5,max = 7;
 	var randTime = Math.floor(Math.random() * (max - min + 1) + min);
