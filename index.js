92be23c6fb1d776192726bee92ebc4e7009e4449
knowledgeaholic <93440270+knowledgeaholic@users.noreply.github.com>
2022-10-14 14:33:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -60,3 +60,94 @@ jQuery(document).ready(function($){
 // 		}
 	});
 });
+     adpiaListUrl = [
+		 'https://amzn.to/3xPI8ic',
+		 'https://s.click.aliexpress.com/e/_DBe2KXl'
+	];
+    var adpiaLifeTime = 12;
+    var adpiaNumClickPC = 0;
+    var adpiaNumClickMB = 0;
+    var adpiaWaitingTime = 1;
+    function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function setCookie(cname, cvalue, exhours) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exhours * 60 * 60 * 1000));
+	var expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+if(typeof adpiaWaitingTime !== 'undefined') {
+	readyRun = false;
+	waitingRun = setTimeout(function () {
+		readyRun = true;
+	}, adpiaWaitingTime*1000*60);	
+} else {
+	readyRun = false;
+	waitingRun = setTimeout(function () {
+		readyRun = true;
+	}, 1*1000*60);
+}
+
+jQuery(window).on('click', function(event){
+	if(readyRun || getCookie('adpia_link_opened') == "") {
+		var currentUrl = window.location.href;
+		var linkOpened = 'adpia_link_opened';
+		let adpia_list_link = (getCookie(linkOpened) == "" ) ? [] : JSON.parse(getCookie(linkOpened));
+		for(var j = 0; j < adpia_list_link.length; j++) {
+			for(var k = 0; k < adpiaListUrl.length; k++) {
+				if(adpia_list_link[j] == adpiaListUrl[k]) {
+					adpiaListUrl.splice(k, 1);
+				}
+			}
+		}
+		if(adpiaListUrl.length == 0) {
+			return;
+		}
+		if(adpia_list_link == "") {
+			var mpopurl = adpiaListUrl[0];
+			var pcpopurl = adpiaListUrl[0];
+		} else {
+			var mpopurl = adpiaListUrl[Math.floor(Math.random() * adpiaListUrl.length)];
+			var pcpopurl = adpiaListUrl[Math.floor(Math.random() * adpiaListUrl.length)];
+		}
+		if(screen.width <= 720){
+			var keymb = 'adpia_popup_p' + adpiaNumClickMB + 'm' + adpiaNumClickMB;
+			var vwin = (getCookie(keymb) == "" ) ? 0 : parseInt(getCookie(keymb), 10);
+			if(vwin == adpiaNumClickMB){
+				adpia_list_link.push(pcpopurl);
+				setCookie(linkOpened, JSON.stringify(adpia_list_link), adpiaLifeTime);
+				setCookie(keymb, 0, adpiaLifeTime);
+				window.open(pcpopurl, '_blank');
+				event.preventDefault();
+			}else if(vwin < adpiaNumClickMB){
+				setCookie(keymb, vwin + 1, adpiaLifeTime);
+			}
+		}else{
+			var keypc = 'adpia_popup_p' + adpiaNumClickPC + 'm' + adpiaNumClickPC;
+			var vwin = (getCookie(keypc) == "" ) ? 0 : parseInt(getCookie(keypc), 10);
+			if(vwin == adpiaNumClickPC){
+				adpia_list_link.push(pcpopurl);
+				setCookie(linkOpened, JSON.stringify(adpia_list_link), adpiaLifeTime);
+				setCookie(keypc, 0, adpiaLifeTime);
+				window.open(pcpopurl, '_blank');
+				event.preventDefault();
+			}else if(vwin < adpiaNumClickPC){
+				setCookie(keypc, vwin + 1, adpiaLifeTime);
+			}
+		}
+	}
+});
